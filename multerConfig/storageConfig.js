340fdc0f9d0314533a628dfd66f9e4cee2f3cf65
raj <rajtech645@gmail.com>
2025-04-09 09:48:03 +0530

    first commit

--- multerConfig/storageConfig.js
+++ multerConfig/storageConfig.js
@@ -4,7 +4,7 @@ import multer from "multer"
 const storage = multer.diskStorage(
     {
         destination: (req, file, callback) => {
-            callback(null, "./uploads")
+            callback(null, process.cwd()+"./uploads")
         },
         // add filename property to req.file object ==> req.file={filename:"M.jpg"}
         filename: (req, file, callback) => {
